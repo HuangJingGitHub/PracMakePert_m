bad3d421b11b8f9f253d067cf2eb42a9179aacf0
Jing HUANG <35371389+HuangJingGitHub@users.noreply.github.com>
2018-08-13 18:50:46 +0800

    Add files via upload

--- Atan2.m
+++ Atan2.m
@@ -1,19 +1,25 @@
-function ang = Atan2(x,y)
-%Adjusted atan() function
-if x==0 && y>0
-    ang = inf;
-elseif x==0 && y<0
-    ang = -inf;
-elseif x==0 && y==0
-    ang = NaN;
-end
+function ang = Atan2(y,x,angunit)
+% Adjusted atan(). The value range is extended to(-pi,pi] according to 
+% the signs of the 2 attributes y and x.
 
-if x>0 && y>0
+if x==0 && y>0
+   ang = 90;
+elseif x==0 && y<0
+   ang = -90;
+elseif x==0 && y==0
+   ang = NaN;
+end
+  
+if x>0 && y>=0
     ang = atan(y/x)/pi*180;
-elseif x<0 && y>0
+elseif x<0 && y>=0
     ang = atan(y/x)/pi*180 + 180;
 elseif x<0 && y<0
     ang = atan(y/x)/pi*180 - 180;
 elseif x>0 && y<0
     ang = atan(y/x)/pi*180;
+end
+
+if nargin == 3 && strcmp(angunit, 'rad')
+    ang = ang / 180 * pi;
 end
