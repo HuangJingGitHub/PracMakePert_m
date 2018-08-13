24f6651740e771ed17bde87aa215b6aa0bb2ff4b
Jing HUANG <35371389+HuangJingGitHub@users.noreply.github.com>
2018-08-13 17:02:55 +0800

    Update Intro2Robotics_2B1.m

--- Intro2Robotics_2B1.m
+++ Intro2Robotics_2B1.m
@@ -20,9 +20,9 @@ Pa = Tba * [Pb; 1];
 %%%Part c)
 Tba_inv = inv(Tba);
 disp('I - Tba_inv*Tba =');
-Result = eye(4) - Tba_inv * Tba
+Result = eye(4) - Tba_inv * Tba;
 disp('I - Tba*Tba_inv =');
-Result = eye(4) - Tba * Tba_inv
+Result = eye(4) - Tba * Tba_inv;
 
 %%%Part d)
 Tcb = transl([3 0 1]) * rpy2tr(0, 20*DR, 0);
