02bb85e29a23ac35be7e2af2e2ab7ea4c11296a0
Jing HUANG <35371389+HuangJingGitHub@users.noreply.github.com>
2018-08-13 21:25:39 +0800

    Add files via upload

--- Atan2.m
+++ Atan2.m
@@ -1,16 +1,14 @@
+%%% Adjusted atan(). The value range is extended to(-pi,pi] according to 
+%%% the signs of the 2 attributes y and x. The default return unit is degree.
+%%% Refer to https://en.wikipedia.org/wiki/Atan2 for more info.
 function ang = Atan2(y,x,angunit)
-% Adjusted atan(). The value range is extended to(-pi,pi] according to 
-% the signs of the 2 attributes y and x.
-
 if x==0 && y>0
    ang = 90;
 elseif x==0 && y<0
    ang = -90;
 elseif x==0 && y==0
    ang = NaN;
-end
-  
-if x>0 && y>=0
+elseif x>0 && y>=0
     ang = atan(y/x)/pi*180;
 elseif x<0 && y>=0
     ang = atan(y/x)/pi*180 + 180;
