4f4dee37474bdb7d0355db34f40b3979aaf61e95
Jing HUANG <35371389+HuangJingGitHub@users.noreply.github.com>
2018-09-29 14:58:13 +0800

    Add files via upload

---
+++ fkSimVis/fkSimVis_main.m
@@ -0,0 +1,120 @@
+%%% Forward kinematics and visualization for manipulator.
+%%% HUANG Jing @ CUHK MAE
+
+clear
+clc
+close all
+
+syms L q1 q2 q3 q4 q5 q6;           % Define the linkage length and joint angles needed.
+D2R = pi/180;                       % Degree to radian constant.
+
+%%% DH table example: PRRRRP robot
+DH(1,:) = [ 0     0   q1         pi/2];
+DH(2,:) = [pi/2   0   0          q2  ];
+DH(3,:) = [-pi/2  L   0          q3  ];
+DH(4,:) = [pi/2   0   0          q4+pi/2];
+DH(5,:) = [pi/4   0   sqrt(2)*L  q5  ];
+DH(6,:) = [pi/4   0   L+q6       pi/2];
+DH(7,:) = [pi/2   0   0          0   ];
+
+%%% Transformation from frame{0} to the last joint
+T1 = eye(4);
+T3 = eye(4);
+T5 = eye(4);
+T7 = eye(4);
+for i=1:7
+    alp = DH(i,1);
+    a   = DH(i,2);
+    d   = DH(i,3);
+    the = DH(i,4);
+    Ti = [cos(the)          -sin(the)         0         a;
+          sin(the)*cos(alp) cos(the)*cos(alp) -sin(alp) -sin(alp)*d;
+          sin(the)*sin(alp) cos(the)*sin(alp) cos(alp)  cos(alp)*d;
+          0                 0                 0         1];
+    T7 = T7*Ti;
+    if i < 2
+        T1 = T7;
+    end
+    
+    if i < 4;
+        T3 = T7;
+    end
+    
+    if i < 6  % i ~= 6 && i ~= 7
+        T5 = T7;
+    end
+end
+T1 = subs(T1(1:3,4),[L q3 q4],[1 0 0]);  % For the joint, just the Cartesian coordinates are needed, 
+T4 = -L*T3(1:3,2)+T3(1:3,4);             % the rotation part won't be used.Set q3,q4 as 0 in this example.
+T3 = subs(T3(1:3,4),[L q3 q4],[1 0 0]);  % Actually, the 4th column of transformation matrix just represents
+T4 = subs(T4,[L q3 q4],[1 0 0]);         % the position of the frame, not necessaryly is the actual position
+T5 = subs(T5(1:3,4),[L q3 q4],[1 0 0]);  % of the joint. That's why the T4 is obtained differently.
+T7 = subs(T7,[L q3 q4],[1 0 0 ]);        % For tip, the oritation part need to be keeped.
+
+%%% Simulation configurations
+dt = 0.02;
+Time  = 5;
+t = 0:dt:Time;
+steps = length(t);
+Q1 = 0.1 * sin(2*pi*t);
+Q2 = pi / 6 * sin(2*pi*t);
+Q3 = zeros(1,steps);
+Q4 = zeros(1,steps);
+Q5 = Q2;
+Q6 = Q1;
+
+X_1 = zeros(3,steps);     % X_1 stores Cartesian coordinate of the second last joint(joint5).
+X_3 = zeros(3,steps); 
+X_4 = zeros(3,steps); 
+X_5 = zeros(3,steps); 
+X   = zeros(3,steps);     % X stores Cartesian coordinate of the end-effector/tip.
+Ori = zeros(3,3*steps);   % Ori stores the ratation matrix of the end-effector.
+
+for i = 1:steps           % Traverse all the time point.
+    Tt = subs(T7,[q1 q2 q5 q6],[Q1(i) Q2(i) Q5(i) Q6(i)]);
+    Tt = double(Tt);
+    X(:,i) = Tt(1:3,4);
+    X_1(:,i)= double(subs(T1,[q1 q2 q5 q6],[Q1(i) Q2(i) Q5(i) Q6(i)]));
+    X_3(:,i)= double(subs(T3,[q1 q2 q5 q6],[Q1(i) Q2(i) Q5(i) Q6(i)]));
+    X_4(:,i)= double(subs(T4,[q1 q2 q5 q6],[Q1(i) Q2(i) Q5(i) Q6(i)]));
+    X_5(:,i)= double(subs(T5,[q1 q2 q5 q6],[Q1(i) Q2(i) Q5(i) Q6(i)]));
+    Ori(:,3*i-2:3*i) = Tt(1:3,1:3);
+end
+
+fkVis(steps,dt,Ori,X,X_1,X_3,X_4,X_5);
+ 
+%%%******Plot the all joints******%%%
+%%% It's noticed that if this figure part is placed before the tip
+%%% trajectory plot part. Then error will be caused in invocation 
+%%% of arrow3. This may be caused by the unknown figure handle conflict.
+%%%
+ figure('Position',[0 -200 1050 800]);
+ H1 = subplot(3,2,1);
+ plot(t, Q1,'k','linewidth',2);
+ xlabel('t \it{(s)}');
+ ylabel('q_1 \it{(m)}');
+ 
+ H2 = subplot(3,2,2);
+ plot(t, Q2/D2R,'k','linewidth',2);
+ xlabel('t \it{(s)}');
+ ylabel('q_2 \it{(deg)}');
+ 
+ H3 = subplot(3,2,3);
+ plot(t, Q3/D2R,'k','linewidth',2);
+ xlabel('t \it{(s)}');
+ ylabel('q_3 \it{(deg)}');
+ 
+ H4 = subplot(3,2,4);
+ plot(t, Q4/D2R,'k','linewidth',2)
+ xlabel('t \it{(s)}');
+ ylabel('q_4 \it{(deg)}');
+ 
+ H5 = subplot(3,2,5);
+ plot(t, Q5/D2R,'k','linewidth',2)
+ xlabel('t \it{(s)}');
+ ylabel('q_5 \it{(deg)}');
+ 
+ H6 = subplot(3,2,6);
+ plot(t, Q6,'k','linewidth',2)
+ xlabel('t \it{(s)}');
+ ylabel('q_6 \it{(m)}');
---
+++ fkSimVis/fkVis.m
@@ -0,0 +1,89 @@
+function fkVis(steps,dt,Ori,X,X_1,X_3,X_4,X_5)
+%%% Forward kinematics visualization / animation.
+
+ratio = 0.6;              % ratio is set for the length adjustment of arrow for better visual effect.
+Ori = ratio * Ori;
+Ori_x = [X(1,1)+Ori(1,1), X(2,1)+Ori(2,1),X(3,1)+Ori(3,1)];    % The arrow end position.
+Ori_y = [X(1,1)+Ori(1,2), X(2,1)+Ori(2,2),X(3,1)+Ori(3,2)];
+Ori_z = [X(1,1)+Ori(1,3), X(2,1)+Ori(2,3),X(3,1)+Ori(3,3)];
+ 
+%%%******Plot the tip trajectory******%%%
+figure('Position',[0 -200 1050 800]);
+plot3(X(1,:),X(2,:),X(3,:));
+axis([0,4 0,4, -2,2]);
+grid on;
+xlabel('X');
+ylabel('Y');
+zlabel('Z');
+title('Tip Trajectory')
+% view(70,35);
+hlink1 = line([0, X_1(1,1)],...
+              [0, X_1(2,1)],...
+              [0, X_1(3,1)],...
+              'Color',[0.67 0 1],'Marker','.','MarkerSize',20, 'LineWidth',2);
+hlink2 = line([X_1(1,1), X_3(1,1)],...
+              [X_1(2,1), X_3(2,1)],...
+              [X_1(3,1), X_3(3,1)],...
+              'Color',[0.67 0 1],'Marker','.','MarkerSize',20, 'LineWidth',2);
+hlink3 = line([X_3(1,1), X_4(1,1)],...
+              [X_3(2,1), X_4(2,1)],...
+              [X_3(3,1), X_4(3,1)],...
+              'Color',[0.67 0 1],'Marker','.','MarkerSize',20, 'LineWidth',2);
+hlink4 = line([X_4(1,1), X_5(1,1)],...
+              [X_4(2,1), X_5(2,1)],...
+              [X_4(3,1), X_5(3,1)],...
+              'Color',[0.67 0 1],'Marker','.','MarkerSize',20, 'LineWidth',2);
+hlink = line([X_5(1,1), X(1,1)],...
+             [X_5(2,1), X(2,1)],...
+             [X_5(3,1), X(3,1)],...
+             'Color',[0.67 0 1],'Marker','.','MarkerSize',20, 'LineWidth',2);
+hold on
+arrow_x = arrow3(X(:,1)', Ori_x, 'k');      % Plot the tip frame coordiate using function arrow.  
+arrow_y = arrow3(X(:,1)', Ori_y, 'r');
+arrow_z = arrow3(X(:,1)', Ori_z, 'g');
+Name_x = text(Ori_x(1), Ori_x(2), Ori_x(3), 'X', 'fontsize', 10);
+Name_y = text(Ori_y(1), Ori_y(2), Ori_y(3), 'Y', 'color', 'r', 'fontsize', 10);
+Name_z = text(Ori_z(1), Ori_z(2), Ori_z(3), 'Z', 'color', 'g', 'fontsize', 10);
+
+ for i=2:steps
+      id_x = 3*i - 2;       % Index number for Ori matrix in each loop. 
+      id_y = id_x + 1;
+      id_z = id_x + 2;
+      Ori_x = [X(1,i)+Ori(1,id_x), X(2,i)+Ori(2,id_x), X(3,i)+Ori(3,id_x)];  % Update Ori_x, Ori_y, Ori_z.
+      Ori_y = [X(1,i)+Ori(1,id_y), X(2,i)+Ori(2,id_y), X(3,i)+Ori(3,id_y)];
+      Ori_z = [X(1,i)+Ori(1,id_z), X(2,i)+Ori(2,id_z), X(3,i)+Ori(3,id_z)];
+      
+      delete(arrow_x);
+      delete(arrow_y);
+      delete(arrow_z);
+      
+      % Update the linkages and tip frame.
+      set(hlink1,'xdata',[0, X_1(1,i)],...        
+                 'ydata',[0, X_1(2,i)],...
+                 'zdata',[0, X_1(3,i)]);
+      set(hlink2,'xdata',[X_1(1,i), X_3(1,i)],...
+                 'ydata',[X_1(2,i), X_3(2,i)],...
+                 'zdata',[X_1(3,i), X_3(3,i)]);
+      set(hlink3,'xdata',[X_3(1,i), X_4(1,i)],...
+                 'ydata',[X_3(2,i), X_4(2,i)],...
+                 'zdata',[X_3(3,i), X_4(3,i)]);
+      set(hlink4,'xdata',[X_4(1,i), X_5(1,i)],...
+                 'ydata',[X_4(2,i), X_5(2,i)],...
+                 'zdata',[X_4(3,i), X_5(3,i)]);   
+      set(hlink,'xdata',[X_5(1,i), X(1,i)],...
+                'ydata',[X_5(2,i), X(2,i)],...
+                'zdata',[X_5(3,i), X(3,i)]);
+  
+      arrow_x = arrow3(X(:,i)' ,Ori_x, 'k');
+      arrow_y = arrow3(X(:,i)' ,Ori_y, 'r');
+      arrow_z = arrow3(X(:,i)' ,Ori_z, 'g');
+      set(Name_x, 'position', Ori_x);
+      set(Name_y, 'position', Ori_y);
+      set(Name_z, 'position', Ori_z);
+      
+      % drawnow;
+      pause(dt*5);       % Pause in every step.
+ end
+ 
+end
+ 
