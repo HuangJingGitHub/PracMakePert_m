fc970a6f114e2a32987d5c9c8b245f09bde0d372
Jing HUANG <35371389+HuangJingGitHub@users.noreply.github.com>
2019-05-13 22:55:28 +0800

    Update Atan2.m

--- Atan2.m
+++ Atan2.m
@@ -1,7 +1,7 @@
 %%% Adjusted atan(). The value range is extended to(-pi,pi] according to 
 %%% the signs of the 2 attributes y and x. The default return unit is degree.
 %%% Refer to https://en.wikipedia.org/wiki/Atan2 for more info.
-function ang = Atan2(y,x,angunit)
+function ang = Atan2(y,x,angUnit)
 if x==0 && y>0
    ang = 90;
 elseif x==0 && y<0
@@ -18,6 +18,6 @@ elseif x>0 && y<0
     ang = atan(y/x)/pi*180;
 end
 
-if nargin == 3 && strcmp(angunit, 'rad')
+if nargin == 3 && strcmp(angUnit, 'rad')
     ang = ang / 180 * pi;
 end
