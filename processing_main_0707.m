ab10ff73a8838027834548c27e403896ebcbd647
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2021-08-06 21:03:15 +0800

    Add files via upload

--- processing_main_0707.m
+++ processing_main_0707.m
@@ -27,6 +27,8 @@ DOToObsDOPt = zeros(2, dataNum);
 DOToObsObsPt = zeros(2, dataNum);
 projectionOnPathPt = zeros(2, dataNum);
 Jd = zeros(2, dataNum * 2);
+obsVertices = [293.7, 313.9, 311.9 291.6; ...
+                149.9, 147.8, 230.9, 230.9];
 
 targetPt = [rawData(3, 1); rawData(4, 1)];
 for i = 1 : dataNum
@@ -58,6 +60,8 @@ for i = 2 : pathSampleNum
 end
 
 pathLengthTracking = zeros(dataNum, 1);
+absDisToTarget = zeros(dataNum, 1);
+closestDisToPath = zeros(dataNum, 1);
 forwardShift = fix(pathSampleNum / 20);
 coordinateError = 0.1;
 for i = 1 : dataNum
@@ -71,69 +75,104 @@ for i = 1 : dataNum
         end       
     end
     pathLengthTracking(i, 1) = accumulatedPathLength(end) - accumulatedPathLength(projectionPtPathIdx, 1);
+    absDisToTarget(i, 1) = norm(feedbackPt(:, i) - targetPt);
+    closestDisToPath(i, 1) = closestDistoPathFunc(feedbackPt(:, i), pathDataXY);
 end
 
 ptToPathProjection = zeros(dataNum, 1);
+eeToObsDistance = zeros(dataNum, 1);
 DOToObsDistance = zeros(dataNum, 1);
+enterIdx = [];
+leaveIdx = [];
 for i = 1 : dataNum
     ptToPathProjection(i, 1) = norm(feedbackPt(:, i) - projectionOnPathPt(:, i));
-    DOToObsDistance(i, 1) = norm(DOToObsDOPt(:, i) - DOToObsObsPt(:, i)); 
+    DOToObsDistance(i, 1) = norm(DOToObsDOPt(:, i) - DOToObsObsPt(:, i));
+    eeToObsDistance(i, 1) = calPtDisToPolygon(eePt(:, i), obsVertices);
+    if i > 1
+        if eeToObsDistance(i, 1) < 0.5 && eeToObsDistance(i - 1, 1) > 0.5
+            enterIdx = [enterIdx i];
+        end
+        if eeToObsDistance(i, 1) > 0.5 && eeToObsDistance(i - 1, 1) < 0.5
+            leaveIdx = [leaveIdx i];
+        end
+    end    
+end
+enterIdx = min(enterIdx);
+leaveIdx = max(leaveIdx);
+for i = enterIdx : leaveIdx
+    eeToObsDistance(i) = 0;
 end
 
-%% plot
 timeSeries = (1 : dataNum) * 0.045;
-fig1 = figure('position', [100 50 700 520]);
-plot(pathDataXY(1, :), pathDataXY(2,:), '--m', 'Linewidth', 2)
-hold on
-plot(feedbackPt(1, :), feedbackPt(2, :), 'Color', [0.8500, 0.3250, 0.0980], 'LineWidth', 2)
-hold on
-plot(pathDataXY(1,1), pathDataXY(2,1), '-o','MarkerSize',10,...
-    'MarkerEdgeColor','r',...
-    'MarkerFaceColor','r')
-hold on
-plot(pathDataXY(1,end), pathDataXY(2,end), '-o','MarkerSize',10,...
-    'MarkerEdgeColor','g',...
-    'MarkerFaceColor','g')
-legend('Path Reference', 'Real Path', 'Location', 'northwest')
+%% plot 1
+% fig1 = figure('position', [500 500 700 400]);
+% plot(pathDataXY(1, :), pathDataXY(2,:), '--m', 'Linewidth', 2)
+% hold on
+% plot(feedbackPt(1, :), feedbackPt(2, :), 'Color', [0.8500, 0.3250, 0.0980], 'LineWidth', 2)
+% hold on
+% plot(pathDataXY(1,1), pathDataXY(2,1), '-o','MarkerSize',10,...
+%     'MarkerEdgeColor','r',...
+%     'MarkerFaceColor','r')
+% hold on
+% plot(pathDataXY(1,end), pathDataXY(2,end), '-o','MarkerSize',10,...
+%     'MarkerEdgeColor','g',...
+%     'MarkerFaceColor','g')
+% text(pathDataXY(1,1) - 1.8, pathDataXY(2, 1) + 6, '\uparrow', 'Fontsize', 14)
+% text(pathDataXY(1,1) - 7.5, pathDataXY(2, 1) + 12, 'start', 'Fontsize', 14)
+% text(pathDataXY(1,end) - 1.8, pathDataXY(2, end) + 6, '\uparrow', 'Fontsize', 14)
+% text(pathDataXY(1,end) - 9, pathDataXY(2, end) + 12, 'target', 'Fontsize', 14)
+% legend('Reference Path', 'Real Path', 'Location', 'northwest')
+% xlabel('x (px)','Fontname','Times','Fontsize', 14);
+% ylabel('y (px)', 'Fontname', 'Times','Fontsize', 14);
+% ax = gca;
+% ax.FontSize = 14;
+% ax.FontName = 'Times';
+% set(ax, 'Ydir', 'reverse')
+% grid on
+% axis equal
+% axis([200 400 200 320])
+% xticks(200:40:400)
+% yticks(200:40:320)
+% print(fig1, './Figure/PathTracking_0707_2338', '-depsc')
 
-xlabel('x (px)','Fontname','Times','Fontsize',14);
-ylabel('y (px)', 'Fontname', 'Times','Fontsize', 14);
-ax = gca;
-ax.FontSize = 14;
-ax.FontName = 'Times';
-set(ax, 'Ydir', 'reverse')
-grid on
-axis equal
-axis([200 400 200 320])
-% xticks(250:10:310)
-% yticks(200:10:250)
-%print(fig1, './figure/SignlePtTrajectory_0501_464', '-depsc')
-
-fig2 = figure('position', [741.6667 -280.3333 868.0000 852.6667]);
+%% plot 2
+fig2 = figure('position', [955 144 774 720]);
 H11 = subplot(3,1,1);
-plot(timeSeries, pathLengthTracking, 'color', 'b', 'Linewidth', 2)
-ylabel('Path Diatance to Target (px)', 'Fontname', 'Times', 'Fontsize', 12)
-%set(gca, 'XTicklabel', []);
-grid on
-gca.FontSize = 16;
-gca.FontName = 'Times';
-% axis([0 22 0 80])
-
-H12 = subplot(3,1,2);
-plot(timeSeries, ptToPathProjection, 'color', 'b', 'Linewidth', 2)
-ylabel('Feedback to Path (px)', 'Fontname', 'Times', 'Fontsize', 12)
+plot(timeSeries, ptToPathProjection, 'Linewidth', 2)
+hold on
+plot(timeSeries, closestDisToPath, 'Linewidth', 2)
+ylabel('Path Err. (px)', 'Fontname', 'Times', 'Fontsize', 14)
+xlabel('(a)', 'Fontname', 'Times', 'Fontsize', 14)
+legend('Tracking error norm', 'Dis. to path', 'Fontname', 'Times', 'Fontsize', 12)
 %set(gca, 'XTicklabel', [])
 grid on
-gca.FontSize = 16;
 gca.FontName = 'Times';
-% axis([0 22 15 30])
+gca.FontSize = 18;
+xlim([0 38])
+
+
+H12 = subplot(3,1,2);
+plot(timeSeries, pathLengthTracking, 'Linewidth', 2)
+hold on
+plot(timeSeries, absDisToTarget, 'LineWidth', 2)
+ylabel('Target Err. (px)', 'Fontname', 'Times', 'Fontsize', 14)
+xlabel('(b)', 'Fontname', 'Times', 'Fontsize', 14)
+legend('In planned path', 'In configuration space', 'Fontname', 'Times', 'Fontsize', 12)
+%set(gca, 'XTicklabel', []);
+grid on
+gca.FontSize = 18;
+gca.FontName = 'Times';
+xlim([0 38])
 
 H13 = subplot(3,1,3);
-plot(timeSeries, DOToObsDistance, 'color', 'b', 'Linewidth', 2)
-xlabel('Time (s)', 'Fontname', 'Times', 'Fontsize', 12)
-ylabel('DO-Obstacle (px)', 'Fontname', 'Times', 'Fontsize', 12)
-gca.FontSize = 16;
+plot(timeSeries, DOToObsDistance, 'Linewidth', 2)
+hold on
+plot(timeSeries, eeToObsDistance, 'Linewidth', 2)
+xlabel({'Time (s)'; '(c)'}, 'Fontname', 'Times', 'Fontsize', 14)
+ylabel('Obstacle Dis. (px)', 'Fontname', 'Times', 'Fontsize', 14)
+legend('DO-obstacle', 'ee-obstacle', 'Fontname', 'Times', 'Fontsize', 12, 'position', [0.54 0.2566 0.1705 0.0590]);
+gca.FontSize = 18;
 gca.FontName = 'Times';
-% axis([0 22 0 20])
 grid on
-%print(fig2, './figure/SignlePt_error_L_d_0501_464', '-depsc')
+xlim([0 38])
+print(fig2, './Figure/DataAnalysis_0707_2338', '-depsc')
